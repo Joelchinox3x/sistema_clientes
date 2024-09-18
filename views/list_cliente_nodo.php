d7e514686e5e23c3f6bd6e6fda0a3c5ef7074d37
joelchino <angichi@gmail.com>
2024-09-18 02:56:35 -0500

    Update list_cliente_nodo.php

--- views/list_cliente_nodo.php
+++ views/list_cliente_nodo.php
@@ -4,14 +4,18 @@
 <div class="container mt-5">
     <div class="row justify-content-center">
         <div class="col-md-10">
-            <?php if (isset($error_message)): ?>
-                <div class="alert alert-warning">
-                    <?= $error_message; ?>
-                </div>
-            <?php endif; ?>
+            <!-- Saludo personalizado -->
+            <h2 class="mb-4 text-center">Hola, <?= $_SESSION['username']; ?>, estos son los clientes de tu nodo:</h2>
+             <!-- Verificar si hay clientes asociados al nodo -->
+            
+   
+ 
+   
+            <!-- Mapa -->
+            <div id="map" style="height: 600px; width: 100%;" class="mb-4"></div>
 
+             <!-- Mostrar la tabla de clientes si existen -->
             <h2 class="mb-4 text-center">Lista de Clientes del Nodo</h2>
-
             <table class="table table-striped">
                 <thead>
                     <tr>
@@ -47,3 +51,27 @@
 
 <?php include('partials/footer.php'); ?>
 
+<!-- Incluir Leaflet para el mapa -->
+<link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
+<script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
+
+<!-- Mapa y marcadores para los nodos -->
+<script>
+    <?php if (!empty($nodos)): ?>
+    var nodos = <?php echo json_encode($nodos); ?>;
+    
+    // Inicializar el mapa en una ubicación predeterminada
+    var map = L.map('map').setView([nodos[0].latitud, nodos[0].longitud], 13);
+
+    // Agregar capa de OpenStreetMap
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    // Añadir marcadores para cada nodo
+    nodos.forEach(function(nodo) {
+        var marker = L.marker([nodo.latitud, nodo.longitud]).addTo(map);
+        marker.bindPopup("<b>Nombre del Nodo:</b> " + nodo.nombre + "<br><b>Latitud:</b> " + nodo.latitud + "<br><b>Longitud:</b> " + nodo.longitud);
+    });
+    <?php endif; ?>
+</script>
