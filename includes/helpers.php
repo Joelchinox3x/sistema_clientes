cc4cf1b79b7fb3cdd82da442fc656c37767b0f73
joelchino <angichi@gmail.com>
2024-09-17 19:54:05 -0500

    Update helpers.php

--- includes/helpers.php
+++ includes/helpers.php
@@ -19,3 +19,14 @@ function calculateDistance($lat1, $lon1, $lat2, $lon2) {
 
     return $distance;
 }
+
+function calculateDistance2($lat1, $lon1, $lat2, $lon2) {
+    $earthRadius = 6371; // Radio de la Tierra en kilómetros.
+    $dLat = deg2rad($lat2 - $lat1);
+    $dLon = deg2rad($lon2 - $lon1);
+    $a = sin($dLat / 2) * sin($dLat / 2) + cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * sin($dLon / 2) * sin($dLon / 2);
+    $c = 2 * atan2(sqrt($a), sqrt(1 - $a));
+    $distance = $earthRadius * $c; // La distancia en kilómetros.
+
+    return $distance;
+}
