becfd40b3019cf50af59d06c084bdfe0c2aeadaf
joelchino <angichi@gmail.com>
2024-09-17 19:50:34 -0500

    Update database.php

--- config/database.php
+++ config/database.php
@@ -3,7 +3,7 @@ class Database {
     private $host = "localhost";
     private $db_name = "clientes_sistema";
     private $username = "root";
-    private $password = "";
+    private $password = "123";
     public $conn;
 
     public function getConnection() {
