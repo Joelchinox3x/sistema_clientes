f4c5bf132c6cc12627e96c8ce1cb2d482ff0813b
joelchino <angichi@gmail.com>
2024-09-17 19:51:32 -0500

    Create NodoDTO.php

---
+++ dto/NodoDTO.php
@@ -0,0 +1,59 @@
+<?php
+class NodoDTO {
+    private $id;
+    private $nombre;
+    private $latitud;
+    private $longitud;
+    private $username;  // Agregamos el atributo username
+
+    public function __construct($nombre, $latitud, $longitud, $id = null, $username = null) {
+        $this->id = $id;
+        $this->nombre = $nombre;
+        $this->latitud = $latitud;
+        $this->longitud = $longitud;
+        $this->username = $username; // Inicializamos el username si es proporcionado
+    }
+
+    // Getters y Setters para cada atributo
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getNombre() {
+        return $this->nombre;
+    }
+
+    public function getLatitud() {
+        return $this->latitud;
+    }
+
+    public function getLongitud() {
+        return $this->longitud;
+    }
+
+    public function getUsername() {
+        return $this->username;
+    }
+
+    public function setUsername($username) {
+        $this->username = $username;
+    }
+
+    public function setId($id) {
+        $this->id = $id;
+    }
+
+    public function setNombre($nombre) {
+        $this->nombre = $nombre;
+    }
+
+    public function setLatitud($latitud) {
+        $this->latitud = $latitud;
+    }
+
+    public function setLongitud($longitud) {
+        $this->longitud = $longitud;
+    }
+}
+?>
+
