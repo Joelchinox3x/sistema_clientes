80a8d909ebd946e67a0c5df1e2f5ce9c52e8f431
joelchino <angichi@gmail.com>
2024-09-17 19:52:49 -0500

    Update create.php

--- views/create.php
+++ views/create.php
@@ -7,7 +7,7 @@
             <h2 class="mb-4 text-center">Agregar Cliente</h2>
             <form action="index.php?action=create" method="POST">
                 <div class="form-group">
-                    <label for="cedula">Cédula:</label>
+                    <label for="cedula">DNI:</label>
                     <input type="text" class="form-control" id="cedula" name="cedula" required>
                 </div>
                 <div class="form-group">
@@ -45,29 +45,68 @@
 
 <script>
     // Inicializar el mapa y centrarlo en una ubicación predeterminada
-    var map = L.map('map').setView([4.596922368201758, -74.07242337470963], 13);
+    var map = L.map('map').setView([-13.0692638462217, -76.3799947500229], 17);
 
-    // Agregar la capa de OpenStreetMap
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    var osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
-    }).addTo(map);
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    });
 
-    // Agregar un marcador arrastrable al mapa
-    var marker = L.marker([51.505, -0.09], {draggable: true}).addTo(map);
+    // Capa de Mapbox Streets (necesitarás una clave de API de Mapbox para esto)
+    var mapboxStreets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=YOUR_MAPBOX_ACCESS_TOKEN', {
+        maxZoom: 19,
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1,
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="https://www.mapbox.com/">Mapbox</a>'
+    });
+
+        // Capa de Google Maps Satellite (necesitarás una clave de API de Google)
+        var googleSat = L.tileLayer('https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}&apikey=YOUR_GOOGLE_API_KEY', {
+        maxZoom: 20,
+        attribution: 'Map data &copy; <a href="https://www.google.com/maps">Google</a>'
+    });
+
+    // Capa de Bing Maps (necesitarás una clave de API de Bing)
+    var bingMaps = L.tileLayer('https://dev.virtualearth.net/REST/V1/Imagery/Metadata/Aerial?output=json&key=YOUR_BING_API_KEY', {
+        maxZoom: 22,
+        attribution: 'Map data &copy; <a href="https://www.bing.com/maps">Bing</a>'
+    });
+
+    // Añadir OpenStreetMap como la capa por defecto
+    googleSat.addTo(map);
+
+    // Añadir una capa de control para alternar entre capas
+    var baseMaps = {
+        "OpenStreetMap": osm,
+        "Mapbox Streets": mapboxStreets,
+        "Google Satellite": googleSat,
+        "Bing Maps Aerial": bingMaps
+    };
+
+    // Control de capas
+    L.control.layers(baseMaps).addTo(map);
+
+    var marker0 = L.marker([-13.068470533518973, -76.38100661337376], {draggable: false}).addTo(map);
+    var marker1 = L.marker([-13.068735726614573, -76.38142235577106], {draggable: false}).addTo(map);
+    var marker2 = L.marker([-13.06861031513689, -76.38141833245754], {draggable: false}).addTo(map);
 
     // Actualizar los campos de latitud y longitud cuando se mueva el marcador
-    marker.on('dragend', function(e) {
-        var latlng = marker.getLatLng();
+    marker0.on('dragend', function(e) {
+        var latlng = marker0.getLatLng();
         document.getElementById('latitud').value = latlng.lat;
         document.getElementById('longitud').value = latlng.lng;
     });
 
     // Permitir que el usuario agregue un nuevo marcador haciendo clic en el mapa
     map.on('click', function(e) {
-        marker.setLatLng(e.latlng);
+        marker1.setLatLng(e.latlng);
         document.getElementById('latitud').value = e.latlng.lat;
         document.getElementById('longitud').value = e.latlng.lng;
     });
+
+ 
+
 </script>
 
 <?php include('partials/footer.php'); ?>
