0825f982f190bcc89addb8a98f1e417958784a27
joelchino <angichi@gmail.com>
2024-09-21 18:42:39 -0500

    Update edit_cliente_nodo.php

--- views/edit_cliente_nodo.php
+++ views/edit_cliente_nodo.php
@@ -1,13 +1,6 @@
 <?php include('partials/header.php'); ?>
 <?php include('partials/navbar.php'); ?>
 
-<?php
-if (isset($_GET['id'])) {
-    $controller = new ClienteNodoController();
-    $cliente = $controller->getClienteById($_GET['id']);
-}
-?>
-
 <div class="container mt-5">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -25,43 +18,43 @@ if (isset($_GET['id'])) {
             <!-- Título con el nombre del cliente y el nombre del nodo -->
             <h2 class="mb-4 text-center">Editar Cliente (<?= $nombreCliente; ?>) del Nodo (<?= $nombreNodo; ?>)</h2>
             
-            <form action="index.php?action=update_cliente_nodo&id=<?php echo $cliente['id']; ?>" method="POST">
+            <form action="index.php?action=update_cliente_nodo&id=<?= $cliente['id']; ?>" method="POST">
                 <input type="hidden" name="nodo_id" value="<?= $cliente['nodo_id'] ?>">
                 <div class="form-group">
                     <label for="dni">DNI:</label>
-                    <input type="text" class="form-control" id="dni" name="dni" value="<?php echo $cliente['dni']; ?>" required>
+                    <input type="text" class="form-control" id="dni" name="dni" value="<?= $cliente['dni']; ?>" required>
                 </div>
                 <div class="form-group">
                     <label for="nombres">Nombres:</label>
-                    <input type="text" class="form-control" id="nombres" name="nombres" value="<?php echo $cliente['nombres']; ?>" required>
+                    <input type="text" class="form-control" id="nombres" name="nombres" value="<?= $cliente['nombres']; ?>" required>
                 </div>
                 <div class="form-group">
                     <label for="apellidos">Apellidos:</label>
-                    <input type="text" class="form-control" id="apellidos" name="apellidos" value="<?php echo $cliente['apellidos']; ?>" required>
+                    <input type="text" class="form-control" id="apellidos" name="apellidos" value="<?= $cliente['apellidos']; ?>" required>
                 </div>
                 <div class="form-group">
                     <label for="telefono">Telefono:</label>
-                    <input type="text" class="form-control" id="telefono" name="telefono" value="<?php echo $cliente['telefono']; ?>" required>
+                    <input type="text" class="form-control" id="telefono" name="telefono" value="<?= $cliente['telefono']; ?>" required>
                 </div>
                 <div class="form-group">
                     <label for="direccion">Dirección:</label>
-                    <input type="text" class="form-control" id="direccion" name="direccion" value="<?php echo $cliente['direccion']; ?>" required>
+                    <input type="text" class="form-control" id="direccion" name="direccion" value="<?= $cliente['direccion']; ?>" required>
                 </div>
                 <div class="form-group">
                     <label for="ip_cliente">IP del Cliente:</label>
-                    <input type="text" class="form-control" id="ip_cliente" name="ip_cliente" value="<?php echo $cliente['ip_cliente']; ?>" >
+                    <input type="text" class="form-control" id="ip_cliente" name="ip_cliente" value="<?= $cliente['ip_cliente']; ?>" >
                 </div>
                 <div class="form-group">
                     <label for="latitud">Latitud:</label>
-                    <input type="text" class="form-control" id="latitud" name="latitud" value="<?php echo $cliente['latitud']; ?>" readonly>
+                    <input type="text" class="form-control" id="latitud" name="latitud" value="<?= $cliente['latitud']; ?>" readonly>
                 </div>
                 <div class="form-group">
                     <label for="longitud">Longitud:</label>
-                    <input type="text" class="form-control" id="longitud" name="longitud" value="<?php echo $cliente['longitud']; ?>" readonly>
+                    <input type="text" class="form-control" id="longitud" name="longitud" value="<?= $cliente['longitud']; ?>" readonly>
                 </div>
                 <div class="form-group">
                     <label for="observaciones">Observaciones:</label>
-                    <input type="text" class="form-control" id="observaciones" name="observaciones" value="<?php echo $cliente['observaciones']; ?>" >
+                    <input type="text" class="form-control" id="observaciones" name="observaciones" value="<?= $cliente['observaciones']; ?>" >
                 </div>
                 <div id="map" style="height: 400px; width: 100%;"></div>
                 <br>
@@ -79,7 +72,7 @@ if (isset($_GET['id'])) {
 
 <script>
     // Inicializar el mapa con la ubicación del cliente actual
-    var map = L.map('map').setView([<?php echo $cliente['latitud']; ?>, <?php echo $cliente['longitud']; ?>], 13);
+    var map = L.map('map').setView([<?= $cliente['latitud']; ?>, <?= $cliente['longitud']; ?>], 13);
 
     // Agregar la capa de OpenStreetMap
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -87,8 +80,8 @@ if (isset($_GET['id'])) {
     }).addTo(map);
 
     // Generar el marcador del cliente y hacerlo arrastrable
-    var marker = L.marker([<?php echo $cliente['latitud']; ?>, <?php echo $cliente['longitud']; ?>], {draggable: true}).addTo(map);
-    marker.bindPopup("<b>Cliente: </b><?php echo $cliente['nombres'] . ' ' . $cliente['apellidos']; ?><br><b>Dirección: </b><?php echo $cliente['direccion']; ?>", {
+    var marker = L.marker([<?= $cliente['latitud']; ?>, <?= $cliente['longitud']; ?>], {draggable: true}).addTo(map);
+    marker.bindPopup("<b>Cliente: </b><?= $cliente['nombres'] . ' ' . $cliente['apellidos']; ?><br><b>Dirección: </b><?= $cliente['direccion']; ?>", {
         maxWidth: 200,
         closeButton: false,
         autoClose: false
